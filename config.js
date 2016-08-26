308611908b2d59cdc81c636c5e164081c39aceff
mirkogonewild <mirkogonewild@users.noreply.github.com>
2016-08-26 11:55:13 +0200

    Create config.js
    Example config

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+wykop: {
+key: '',
+secret: '',
+connection: ''
+},
+siteURL: 'https://mirkogonewild.pl',
+mongoURL: 'mongodb://username:password@host:port/database',
+secret: ''
+}
